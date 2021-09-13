572a031c6db12148afd50e9718c6a3ed088067a5
Ivan Pevtsov <pevtsovi@cqg.com>
2021-09-13 11:47:52 +0300

    removed env file usage

--- settings.py
+++ settings.py
@@ -1,13 +1,15 @@
+import os
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    server_host: str = "0.0.0.0"
-    server_port: int = 8000
-    log_level: str = "DEBUG"
-    cf_url = "https://cloudflare-eth.com"
-    memcached_host: str = "memcached"
-    memcached_port: int = 11211
+    server_host: str = os.getenv("HOST", "0.0.0.0")
+    server_port: int = os.getenv("PORT", 8000)
+    log_level: str = os.getenv("LOG_LEVEL", "DEBUG")
+    cf_url = os.getenv("CLOUD_FLARE_URL", "https://cloudflare-eth.com")
+    memcached_host: str = os.getenv("MEMCACHED_HOST", "memcached")
+    memcached_port: int = os.getenv("MEMCACHED_PORT", 11211)
 
 
-settings = Settings(_env_file="/app/.env", _env_file_encoding="UTF-8")
+settings = Settings()
